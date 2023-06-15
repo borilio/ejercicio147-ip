31a80b709d4d4789b756afba99cbb7d8d13c1628
borilio <exprimetuandroid@gmail.com>
2023-06-15 14:12:28 +0200

    Segundo commit final

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import {HttpClient, HttpErrorResponse} from "@angular/common/http";
+import {DireccionIp} from "./models/direccionip.model";
+import {Localizacion} from "./models/localizacion.model";
 
 @Component({
   selector: 'app-root',
@@ -6,5 +9,57 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'ejercicio147';
+
+  public direccionIP!: DireccionIp;
+  public textoBoton: string;
+  public cargando: boolean;
+  public datosLocalizacion!: Localizacion;
+
+
+
+  // TODO 2. Inyectar el servicio httpClient
+
+  constructor(private httpClient: HttpClient) {
+    this.textoBoton = "Obtener IP";
+    this.cargando = false;
+  }
+
+
+  public actualizarIP() {
+    // TODO 3. Usar el servicio httpClient
+    // TODO 4. Suscribirnos al observable que obtenemos de .get()
+    this.textoBoton = "Cargando...";
+    this.cargando = true;
+    this.httpClient.get<DireccionIp>("https://api.ipify.org/?format=json").subscribe(
+      {
+        next: (datos : DireccionIp) => {
+          console.log("Han llegado los datos", datos);
+          this.direccionIP = datos;
+          this.textoBoton = "Obtener IP";
+          this.cargando = false;
+        },
+        error: (error) => {
+          console.error("Se ha producido un error", error);
+          this.cargando = false;
+        }
+      }
+    );
+
+  }
+
+  public actualizarLocalizacion(){
+    const url = `https://ipinfo.io/${this.direccionIP.ip}/geo`;
+    this.httpClient.get<Localizacion>(url).subscribe(
+      {
+        next: (datos : Localizacion) => {
+          console.log("Llegaron los datos", datos);
+          this.datosLocalizacion = datos;
+        },
+        error: (error: HttpErrorResponse) => {
+          console.error(error);
+        }
+      }
+    );
+  }
+
 }
